f6fd3aaa8608da20003b1032384c67dd6e37f3f6
Sean Yu <yusean1792@gmail.com>
2016-01-21 20:57:45 -0800

    Added Some more changes
    Added area calculations and minimum area filters.

--- src/org/usfirst/frc/team948/robot/Robot.java
+++ src/org/usfirst/frc/team948/robot/Robot.java
@@ -9,6 +9,7 @@ import com.ni.vision.NIVision.ShapeMode;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This is a demo program showing the use of the NIVision class to do vision processing. 
@@ -23,7 +24,9 @@ public class Robot extends SampleRobot {
     NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(24, 49);	//Default hue range for yellow tote
 	NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(67, 255);	//Default saturation range for yellow tote
 	NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(49, 255);	//Default value range for yellow tote
-    public void robotInit() {
+	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);
+	public void robotInit() {
 
         frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
         binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
@@ -31,6 +34,7 @@ public class Robot extends SampleRobot {
         session = NIVision.IMAQdxOpenCamera("cam0",
                 NIVision.IMAQdxCameraControlMode.CameraControlModeController);
         NIVision.IMAQdxConfigureGrab(session);
+        criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, 0.5, 100.0, 0, 0);
     }
 
     public void operatorControl() {
@@ -46,12 +50,19 @@ public class Robot extends SampleRobot {
         	//grabs image from the session
             NIVision.IMAQdxGrab(session, frame, 1);
             //fills the binaryFrame with the threshold HSV values
-            NIVision.imaqColorThreshold(binaryFrame, frame, 255, NIVision.ColorMode.RGB, TOTE_HUE_RANGE, TOTE_SAT_RANGE, TOTE_VAL_RANGE);
+            NIVision.imaqColorThreshold(binaryFrame, frame, 255, NIVision.ColorMode.HSV, TOTE_HUE_RANGE, TOTE_SAT_RANGE, TOTE_VAL_RANGE);
             //draws circle on binary image
             NIVision.imaqDrawShapeOnImage(binaryFrame, binaryFrame, rect,
                     DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-            
             CameraServer.getInstance().setImage(binaryFrame);
+            NIVision.imaqParticleFilter4(binaryFrame, binaryFrame, criteria, filterOptions, null);
+            //NUmber of particles
+            int numParticles = NIVision.imaqCountParticles(binaryFrame, 1);
+			SmartDashboard.putNumber("Filtered particles", numParticles);
+			
+            double area = NIVision.imaqMeasureParticle(binaryFrame, 0, 0, NIVision.MeasurementType.MT_AREA);
+            SmartDashboard.putNumber("Area", area);
+            
             
             /** robot code here! **/
             Timer.delay(0.005);		// wait for a motor update time
